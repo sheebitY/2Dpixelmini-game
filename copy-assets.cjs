82f316c18c5c2ac60fb7d5934eb71b5a1af09223
sheebitY <2371173801@qq.com>
2026-07-09 01:42:41 +0800

    fix: use cross-platform copy script for Cloudflare Pages build

---
+++ copy-assets.cjs
@@ -0,0 +1,21 @@
+﻿const fs = require("fs");
+const path = require("path");
+
+function copyDir(src, dest) {
+  if (!fs.existsSync(dest)) fs.mkdirSync(dest, { recursive: true });
+  for (const item of fs.readdirSync(src)) {
+    const s = path.join(src, item);
+    const d = path.join(dest, item);
+    if (fs.statSync(s).isDirectory()) {
+      copyDir(s, d);
+    } else {
+      fs.copyFileSync(s, d);
+    }
+  }
+}
+
+const src = path.resolve(__dirname, "assets");
+const dest = path.resolve(__dirname, "dist", "assets");
+console.log("Copying " + src + " -> " + dest);
+copyDir(src, dest);
+console.log("Done");
